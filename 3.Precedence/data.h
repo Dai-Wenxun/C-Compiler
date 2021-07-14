95a548229809a304d74f30893fe59a2a0e8b68a9
Dai-Wenxun <xfeynman@gmail.com>
2021-07-14 11:49:16 +0800

    v0

---
+++ 3.Precedence/data.h
@@ -0,0 +1,9 @@
+#ifndef extern_
+#define extern_ extern
+#endif
+
+
+extern_ int Line;
+extern_ int	Putback;
+extern_ FILE *Infile;
+extern_ struct token Token;
---
+++ 3.Precedence/interp.c
@@ -0,0 +1,34 @@
+#include "defs.h"
+#include "data.h"
+#include "decl.h"
+
+static char *ASTOP[] = {"+", "-", "*", "/"};
+
+int interpretAST(struct ASTnode *n) {
+    int leftval, rightval;
+    if (n->left)
+        leftval = interpretAST(n->left);
+    if (n->right)
+        rightval = interpretAST(n->right);
+
+    if (n->op == A_INTLIT)
+        printf("int %d\n", n->intvalue);
+    else
+        printf("%d %s %d\n", leftval, ASTOP[n->op], rightval);
+
+    switch (n->op) {
+        case A_ADD:
+            return leftval + rightval;
+        case A_SUBTRACT:
+            return leftval - rightval;
+        case A_MULTIPLY:
+            return leftval * rightval;
+        case A_DIVIDE:
+            return leftval / rightval;
+        case A_INTLIT:
+            return n->intvalue;
+        default:
+            fprintf(stderr, "Unknown AST operator %d\n", n->op);
+            exit(1);
+    }
+}
