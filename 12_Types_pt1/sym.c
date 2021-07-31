1bc4a1763a45b6f026f61cab4a0850629e38a0c2
Dai-Wenxun <wxDai2001@gmail.com>
2021-07-31 14:06:28 +0800

    Latest Update

---
+++ 12_Types_pt1/sym.c
@@ -0,0 +1,34 @@
+#include "defs.h"
+#include "data.h"
+#include "decl.h"
+
+static int Globs = 0;
+
+int findglob(char *s) {
+    for (int i = 0; i < Globs; ++i) {
+        if (*s == *Gsym[i].name && !strcmp(s, Gsym[i].name))
+            return i;
+    }
+    return -1;
+}
+
+static int newglob(void) {
+    int p;
+
+    if ((p = Globs++) >= NSYMBOLS)
+        fatal("Too many global symbols");
+    return p;
+}
+
+int addglob(char *name, int type, int stype) {
+    int y;
+
+    if ((y = findglob(name)) != -1)
+        return y;
+
+    y = newglob();
+    Gsym[y].name = strdup(name);
+    Gsym[y].type = type;
+    Gsym[y].stype = stype;
+    return y;
+}
