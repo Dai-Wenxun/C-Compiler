a5f1c6f4c602af0addcb331c6a1a13f6a8ac7706
Dai-Wenxun <wxDai2001@gmail.com>
2021-08-04 11:47:39 +0800

    Latest Update

---
+++ 17_Scaling_Offsets/cg.c
@@ -0,0 +1,258 @@
+#include "defs.h"
+#include "data.h"
+#include "decl.h"
+
+static int freereg[4];
+static char *reglist[4] = {"%r8", "%r9", "%r10", "%r11"};
+static char *dreglist[4] = {"%r8d", "%r9d", "%r10d", "%r11d"};
+static char *breglist[4] = {"%r8b", "%r9b", "%r10b", "%r11b"};
+
+void freeall_registers(void) {
+    freereg[0] = freereg[1] = freereg[2] = freereg[3] = 1;
+}
+
+static int alloc_register(void) {
+    for (int i = 0; i < 4; i++) {
+        if (freereg[i]) {
+            freereg[i] = 0;
+            return (i);
+        }
+    }
+    fatal("Out of registers");
+}
+
+static void free_register(int reg) {
+    if (freereg[reg] != 0) {
+        fatald("Error trying to free register", reg);
+    }
+    freereg[reg] = 1;
+}
+
+void cgpreamble() {
+    freeall_registers();
+    fputs("\t.text\n", Outfile);
+
+}
+
+void cgpostamble() {
+}
+
+void cgfuncpreamble(int id) {
+    char *name = Gsym[id].name;
+    fprintf(Outfile,
+            "\t.text\n"
+            "\t.globl\t%s\n"
+            "\t.type\t%s, @function\n"
+            "%s:\n"
+            "\tpushq\t%%rbp\n"
+            "\tmovq\t%%rsp, %%rbp\n",
+            name, name, name);
+}
+
+void cgfuncpostamble(int id) {
+    cglabel(Gsym[id].endlabel);
+    fputs(
+        "\tpopq\t%rbp\n"
+        "\tret\n",
+        Outfile);
+}
+
+static char *invcmplist[] =
+        {"jne", "je", "jge", "jle", "jg", "jl"};
+
+int cgcompare_and_jump(int ASTop, int r1, int r2, int label) {
+    if (ASTop < A_EQ || ASTop > A_GE)
+        fatal("Bad ASTop in cgcompare_and_jump()");
+
+    fprintf(Outfile, "\tcmpq\t%s, %s\n", reglist[r2], reglist[r1]);
+    fprintf(Outfile, "\t%s\tL%d\n", invcmplist[ASTop - A_EQ], label);
+    freeall_registers();
+    return (NOREG);
+}
+
+static char *cmplist[] =
+        {"sete", "setne", "setl", "setg", "setle", "setge"};
+
+int cgcompare_and_set(int ASTop, int r1, int r2) {
+    if (ASTop < A_EQ || ASTop > A_GE)
+        fatal("Bad ASTop in cgcompare_and_set()");
+
+    fprintf(Outfile, "\tcmpq\t%s, %s\n", reglist[r2], reglist[r1]);
+    fprintf(Outfile, "\t%s\t%s\n", cmplist[ASTop - A_EQ], breglist[r2]);
+    fprintf(Outfile, "\tmovzbq\t%s, %s\n", breglist[r2], reglist[r2]);
+    free_register(r1);
+    return (r2);
+}
+
+int cgadd(int r1, int r2) {
+    fprintf(Outfile, "\taddq\t%s, %s\n", reglist[r1], reglist[r2]);
+    free_register(r1);
+    return (r2);
+}
+
+int cgsub(int r1, int r2) {
+    fprintf(Outfile, "\tsubq\t%s, %s\n", reglist[r2], reglist[r1]);
+    free_register(r2);
+    return (r1);
+}
+
+int cgmul(int r1, int r2) {
+    fprintf(Outfile, "\timulq\t%s, %s\n", reglist[r1], reglist[r2]);
+    free_register(r1);
+    return (r2);
+}
+
+int cgdiv(int r1, int r2) {
+    fprintf(Outfile, "\tmovq\t%s,%%rax\n", reglist[r1]);
+    fprintf(Outfile, "\tcqo\n");
+    fprintf(Outfile, "\tidivq\t%s\n", reglist[r2]);
+    fprintf(Outfile, "\tmovq\t%%rax,%s\n", reglist[r1]);
+    free_register(r2);
+    return (r1);
+}
+
+int cgloadint(int value, int type) {
+    int r = alloc_register();
+
+    fprintf(Outfile, "\tmovq\t$%d, %s\n", value, reglist[r]);
+    return (r);
+}
+
+int cgloadglob(int id) {
+    int r = alloc_register();
+
+    switch (Gsym[id].type) {
+        case P_CHAR:
+            fprintf(Outfile, "\tmovzbq\t%s(%%rip), %s\n", Gsym[id].name, reglist[r]);
+            break;
+        case P_INT:
+            fprintf(Outfile, "\tmovl\t%s(%%rip), %s\n", Gsym[id].name, dreglist[r]);
+            break;
+        case P_LONG:
+        case P_CHARPTR:
+        case P_INTPTR:
+        case P_LONGPTR:
+            fprintf(Outfile, "\tmovq\t%s(%%rip), %s\n", Gsym[id].name, reglist[r]);
+            break;
+        default:
+            fatald("Bad type in cgloadglob()", Gsym[id].type);
+    }
+    return (r);
+}
+
+int cgstorglob(int r, int id) {
+    switch (Gsym[id].type) {
+        case P_CHAR:
+            fprintf(Outfile, "\tmovb\t%s, %s(%%rip)\n",
+                    breglist[r], Gsym[id].name);
+            break;
+        case P_INT:
+            fprintf(Outfile, "\tmovl\t%s, %s(%%rip)\n",
+                    dreglist[r], Gsym[id].name);
+            break;
+        case P_LONG:
+        case P_CHARPTR:
+        case P_INTPTR:
+        case P_LONGPTR:
+            fprintf(Outfile, "\tmovq\t%s, %s(%%rip)\n",
+                    reglist[r], Gsym[id].name);
+            break;
+        default:
+            fatald("Bad type in cgstorglob()", Gsym[id].type);
+    }
+    return (r);
+}
+
+void cgprintint(int r) {
+    fprintf(Outfile, "\tmovq\t%s, %%rdi\n", reglist[r]);
+    fprintf(Outfile, "\tcall\tprintint\n");
+    free_register(r);
+}
+
+int cgwiden(int r, int oldtype, int newtype) {
+    return (r);
+}
+
+void cgreturn(int r, int id) {
+    switch (Gsym[id].type) {
+        case P_CHAR:
+            fprintf(Outfile, "\tmovzbl\t%s, %%eax\n", breglist[r]);
+            break;
+        case P_INT:
+            fprintf(Outfile, "\tmovl\t%s, %%eax\n", dreglist[r]);
+            break;
+        case P_LONG:
+            fprintf(Outfile, "\tmovq\t%s, %%rax\n", reglist[r]);
+            break;
+        default:
+            fatald("Bad function type in cgreturn()", Gsym[id].type);
+    }
+    cgjump(Gsym[id].endlabel);
+}
+
+int cgcall(int r, int id) {
+    int outr = alloc_register();
+    fprintf(Outfile, "\tmovq\t%s, %%rdi\n", reglist[r]);
+    fprintf(Outfile, "\tcall\t%s\n", Gsym[id].name);
+    fprintf(Outfile, "\tmovq\t%%rax, %s\n", reglist[outr]);
+    free_register(r);
+    return (outr);
+}
+
+int cgaddress(int id) {
+    int r = alloc_register();
+    fprintf(Outfile, "\tleaq\t%s(%%rip), %s\n", Gsym[id].name, reglist[r]);
+
+    return (r);
+}
+
+int cgderef(int r, int type) {
+
+    switch (type) {
+        case P_CHARPTR:
+            fprintf(Outfile, "\tmovzbq\t(%s), %s\n", reglist[r], reglist[r]);
+            break;
+        case P_INTPTR:
+            fprintf(Outfile, "\tmovl\t(%s), %s\n", reglist[r], dreglist[r]);
+            break;
+        case P_LONGPTR:
+            fprintf(Outfile, "\tmovq\t(%s), %s\n", reglist[r], reglist[r]);
+            break;
+        default:
+            fatal("void * can not be dereferenced");
+    }
+    return (r);
+}
+
+int cgshlconst(int r, int val) {
+    fprintf(Outfile, "\tsalq\t$%d, %s\n", val, reglist[r]);
+    return (r);
+}
+
+static int psize[] = {0, 0 , 1, 4, 8, 8, 8, 8, 8};
+int cgprimsize(int type) {
+    if (type < P_NONE || type > P_LONGPTR)
+        fatal("Bad type in cgprimsize()");
+    return (psize[type]);
+}
+
+void cgglobsym(int id) {
+    int typesize;
+
+    typesize = cgprimsize(Gsym[id].type);
+    fprintf(Outfile, "\t.data\n" "\t.globl\t%s\n", Gsym[id].name);
+    switch (typesize) {
+        case 1: fprintf(Outfile, "%s:\t.byte\t0\n", Gsym[id].name); break;
+        case 4: fprintf(Outfile, "%s:\t.long\t0\n", Gsym[id].name); break;
+        case 8: fprintf(Outfile, "%s:\t.quad\t0\n", Gsym[id].name); break;
+        default: fatald("Unknown typesize in cgglobsym()", typesize);
+    }
+}
+
+void cglabel(int l) {
+    fprintf(Outfile, "L%d:\n", l);
+}
+
+void cgjump(int l) {
+    fprintf(Outfile, "\tjmp\tL%d\n", l);
+}
---
+++ 17_Scaling_Offsets/gen.c
@@ -0,0 +1,163 @@
+#include "defs.h"
+#include "data.h"
+#include "decl.h"
+
+int genlabel(void) {
+    static int id = 1;
+    return (id++);
+}
+
+static int genIF(struct ASTnode *n) {
+    int Lfalse, Lend;
+
+    Lfalse = genlabel();
+    if (n->right)
+        Lend = genlabel();
+
+    genAST(n->left, Lfalse, n->op);
+    genfreeregs();
+
+    genAST(n->mid, NOREG, n->op);
+    genfreeregs();
+
+    if (n->right)
+        cgjump(Lend);
+
+    cglabel(Lfalse);
+
+    if (n->right) {
+        genAST(n->right, NOREG, n->op);
+        genfreeregs();
+        cglabel(Lend);
+    }
+
+    return (NOREG);
+}
+
+static int genWHILE(struct ASTnode *n) {
+    int Lstart, Lend;
+
+    Lstart = genlabel();
+    Lend = genlabel();
+
+    cglabel(Lstart);
+
+    genAST(n->left, Lend, n->op);
+    genfreeregs();
+
+    genAST(n->right, NOREG, n->op);
+    genfreeregs();
+
+    cgjump(Lstart);
+    cglabel(Lend);
+
+    return (NOREG);
+}
+
+int genAST(struct ASTnode *n, int reg, int parentASTop) {
+    int leftreg, rightreg;
+
+    switch (n->op) {
+        case A_IF:
+            return (genIF(n));
+        case A_WHILE:
+            return (genWHILE(n));
+        case A_GLUE:
+            genAST(n->left, NOREG, n->op);
+            genfreeregs();
+            genAST(n->right, NOREG, n->op);
+            genfreeregs();
+            return (NOREG);
+        case A_FUNCTION:
+            cgfuncpreamble(n->v.id);
+            genAST(n->left, NOREG, n->op);
+            cgfuncpostamble(n->v.id);
+            return (NOREG);
+    }
+
+    if (n->left)
+        leftreg = genAST(n->left, NOREG, n->op);
+    if (n->right)
+        rightreg = genAST(n->right, leftreg, n->op);
+
+    switch (n->op) {
+        case A_EQ:
+        case A_NE:
+        case A_LT:
+        case A_GT:
+        case A_LE:
+        case A_GE:
+            if (parentASTop == A_IF || parentASTop == A_WHILE)
+                return (cgcompare_and_jump(n->op, leftreg, rightreg, reg));
+            else
+                return (cgcompare_and_set(n->op, leftreg, rightreg));
+
+        case A_ADD:
+            return (cgadd(leftreg, rightreg));
+        case A_SUBTRACT:
+            return (cgsub(leftreg, rightreg));
+        case A_MULTIPLY:
+            return (cgmul(leftreg, rightreg));
+        case A_DIVIDE:
+            return (cgdiv(leftreg, rightreg));
+        case A_INTLIT:
+            return (cgloadint(n->v.intvalue, n->type));
+        case A_IDENT:
+            return (cgloadglob(n->v.id));
+        case A_LVIDENT:
+            return (cgstorglob(reg, n->v.id));
+        case A_ASSIGN:
+            return (rightreg);
+        case A_PRINT:
+            genprintint(leftreg);
+            genfreeregs();
+            return (NOREG);
+        case A_WIDEN:
+            return (cgwiden(leftreg, n->left->type, n->type));
+        case A_RETURN:
+            cgreturn(leftreg, Functionid);
+            return (NOREG);
+        case A_FUNCCALL:
+            return (cgcall(leftreg, n->v.id));
+        case A_ADDR:
+            return (cgaddress(n->v.id));
+        case A_DEREF:
+            return (cgderef(leftreg, n->left->type));
+        case A_SCALE:
+            switch (n->v.size) {
+                case 2: return (cgshlconst(leftreg, 1));
+                case 4: return (cgshlconst(leftreg, 2));
+                case 8: return (cgshlconst(leftreg, 3));
+                default:
+                    rightreg = cgloadint(n->v.size, P_INT);
+                    return (cgmul(leftreg, rightreg));
+            }
+        default:
+            fatald("Unknown AST operator", n->op);
+    }
+}
+
+void genpreamble() {
+    cgpreamble();
+}
+
+void genpostamble() {
+    cgpostamble();
+}
+
+void genfreeregs() {
+    freeall_registers();
+}
+
+void genprintint(int reg) {
+    cgprintint(reg);
+}
+
+void genglobsym(int id) {
+    cgglobsym(id);
+}
+
+int genprimsize(int type) {
+    return (cgprimsize(type));
+}
+
