d8bc3dc0354844633b1b3411b1ec8b5bc5a85c36
Dai-Wenxun <wxDai2001@gmail.com>
2021-08-05 21:55:40 +0800

    Latest Update

---
+++ 19_Arrays_pt1/main.c
@@ -0,0 +1,54 @@
+#include "defs.h"
+#define extern_
+#include "data.h"
+#undef extern_
+#include "decl.h"
+
+static void init() {
+    Line = 1;
+    Putback = '\n';
+    Globs = 0;
+    O_dumpAST = 0;
+}
+
+static void usage(char *prog) {
+    fprintf(stderr, "Usage: %s [-T] infile\n", prog);
+    exit(1);
+}
+
+int main(int argc, char* argv[]) {
+    int i;
+
+    init();
+
+    for (i = 1; i < argc; i++) {
+        if (*argv[i] != '-') break;
+        for (int j = 1; argv[i][j]; j++) {
+            switch (argv[i][j]) {
+                case 'T': O_dumpAST = 1; break;
+                default: usage(argv[0]);
+            }
+        }
+    }
+
+    if (i >= argc)
+        usage(argv[0]);
+
+    if ((Infile = fopen(argv[i], "r")) == NULL) {
+        fprintf(stderr, "Unable to open %s: %s\n", argv[i], strerror(errno));
+        exit(1);
+    }
+    if ((Outfile = fopen("out.s", "w")) == NULL) {
+        fprintf(stderr, "Unable to create out.s: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    addglob("printint", P_CHAR, S_FUNCTION, 0, 0);
+
+    scan(&Token);
+    genpreamble();
+    global_declarations();
+    genpostamble();
+    fclose(Outfile);
+    return (0);
+}
---
+++ 19_Arrays_pt1/scan.c
@@ -0,0 +1,232 @@
+#include "defs.h"
+#include "data.h"
+#include "decl.h"
+
+static int chrpos(char *s, int c) {
+    char *p;
+
+    p = strchr(s, c);
+    return (p ? p-s : -1);
+}
+
+static int next(void) {
+    int c;
+
+    if (Putback) {
+        c = Putback;
+        Putback = 0;
+        return (c);
+    }
+
+    c = fgetc(Infile);
+    if (c == '\n')
+        Line++;
+
+    return (c);
+}
+
+static void putback(int c) {
+    Putback = c;
+}
+
+static int skip(void) {
+    int c;
+
+    c = next();
+
+    while (' ' == c || '\t' == c || '\n' == c || '\f' == c || '\r' == c) {
+        c = next();
+    }
+
+    return (c);
+}
+
+static int scanint(int c) {
+    int k, val = 0;
+
+    while ((k = chrpos("0123456789", c)) >= 0) {
+        val = 10 * val + k;
+        c = next();
+    }
+
+    putback(c);
+    return (val);
+}
+
+static int scanident(int c, char *buf, int lim) {
+    int i = 0;
+
+    while (isalpha(c) || isdigit(c) || '_' == c) {
+        if (lim - 1 == i) {
+            fatal("identifier too long");
+        } else if (i < lim - 1) {
+            buf[i++] = (char)c;
+        }
+        c = next();
+    }
+
+    putback(c);
+    buf[i] = '\0';
+    return (i);
+}
+
+static int keyword(char *s) {
+    switch (*s) {
+        case 'c':
+            if (!strcmp(s, "char"))
+                return (T_CHAR);
+            break;
+        case 'e':
+            if (!strcmp(s, "else"))
+                return (T_ELSE);
+            break;
+        case 'f':
+            if (!strcmp(s, "for"))
+                return (T_FOR);
+            break;
+        case 'i':
+            if (!strcmp(s, "if"))
+                return (T_IF);
+            if (!strcmp(s, "int"))
+                return (T_INT);
+            break;
+        case 'l':
+            if (!strcmp(s, "long"))
+                return (T_LONG);
+            break;
+        case 'r':
+            if (!strcmp(s, "return"))
+                return (T_RETURN);
+            break;
+        case 'v':
+            if (!strcmp(s, "void"))
+                return (T_VOID);
+            break;
+        case 'w':
+            if (!strcmp(s, "while"))
+                return (T_WHILE);
+            break;
+    }
+    return (0);
+}
+
+static struct token *Rejtoken = NULL;
+
+void reject_token(struct token *t) {
+    if (Rejtoken != NULL)
+        fatal("Can't reject token twice");
+    Rejtoken = t;
+}
+
+int scan(struct token *t) {
+    int c, tokentype;
+
+    if (Rejtoken != NULL) {
+        t = Rejtoken;
+        Rejtoken = NULL;
+        return (1);
+    }
+
+    c = skip();
+
+    switch (c) {
+        case EOF:
+            t->token = T_EOF;
+            return (0);
+        case '+':
+            t->token = T_PLUS;
+            break;
+        case '-':
+            t->token = T_MINUS;
+            break;
+        case '*':
+            t->token = T_STAR;
+            break;
+        case '/':
+            t->token = T_SLASH;
+            break;
+        case ';':
+            t->token = T_SEMI;
+            break;
+        case '{':
+            t->token = T_LBRACE;
+            break;
+        case '}':
+            t->token = T_RBRACE;
+            break;
+        case '(':
+            t->token = T_LPAREN;
+            break;
+        case ')':
+            t->token = T_RPAREN;
+            break;
+        case '[':
+            t->token = T_LBRACKET;
+            break;
+        case ']':
+            t->token = T_RBRACKET;
+            break;
+        case ',':
+            t->token = T_COMMA;
+            break;
+        case '=':
+            if ((c=next()) == '=') {
+                t->token = T_EQ;
+            } else {
+                putback(c);
+                t->token = T_ASSIGN;
+            }
+            break;
+        case '!':
+            if ((c=next()) == '=') {
+                t->token = T_NE;
+            } else {
+                fatalc("Unrecognised character", c);
+            }
+            break;
+        case '<':
+            if ((c=next()) == '=') {
+                t->token = T_LE;
+            } else {
+                putback(c);
+                t->token = T_LT;
+            }
+            break;
+        case '>':
+            if ((c=next()) == '=') {
+                t->token = T_GE;
+            } else {
+                putback(c);
+                t->token = T_GT;
+            }
+            break;
+        case '&':
+            if ((c=next()) == '&') {
+                t->token = T_LOGAND;
+            } else {
+                putback(c);
+                t->token = T_AMPER;
+            }
+            break;
+        default:
+            if (isdigit(c)) {
+                t->intvalue = scanint(c);
+                t->token = T_INTLIT;
+                break;
+            } else if (isalpha(c) || '_' == c) {
+                scanident(c, Text, TEXTLEN);
+
+                tokentype = keyword(Text);
+                if (tokentype) {
+                    t->token = tokentype;
+                    break;
+                }
+                // Not a keyword, so must be an identifier.
+                t->token = T_IDENT;
+                break;
+            }
+
+            fatalc("Unrecognised character", c);
+    }
+    return (1);
+}
